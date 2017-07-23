518cb1a84cd801ce5f8ea8198616a0547b5ba020
Peng L <373970862@qq.com>
2017-07-23 09:33:21 +0800

    修复bug

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,10 @@ import Vuex from 'vuex';
 import PlMain from './modules/PlMain';
 import PlDialogCollection from './modules/PlDialogCollection';
 import PlSlideBar from './modules/PlSlideBar';
-import actions from './actions';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	actions,
 	modules:{
 		PlMain,
 		PlDialogCollection,
